3a51e0c1282d98c4c8c0b16f4c0a081370f5bfec
Fang Chen <fangchen@Fangs-MacBook-Air.local>
2025-04-16 15:42:00 -0700

    update

--- references/pipeline_classification/utils/__init__.py
+++
@@ -1,3 +0,0 @@
-from .datasets import *
-from .tools import *
-from .run_manager import *
--- references/pipeline_detection/mmdet_train.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-ROOT=/home/fangchen/WorkDir/Residual-Encoded-Distillation/
-CONFIG=$1
-
-PYTHONPATH="$(dirname $0)/":"$ROOT":$PYTHONPATH \
-python ./mmdetection/tools/train.py $CONFIG
